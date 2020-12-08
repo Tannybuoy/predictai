1e2811a2ba43a696755bd349ab3c67cc5e53a25e
Tannybuoy <jagger1948@gmail.com>
2020-12-08 14:01:35 +0530

    ml model deployed

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, redirect, url_for, render_template, request
+import numpy as np
+import pickle
+
+model=pickle.load(open('iri.pkl', 'rb'))
 
 app=Flask(__name__)
 
@@ -6,13 +10,24 @@ app=Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/login", methods=['POST','GET'])
-def login():
+@app.route("/trial", methods=['GET', 'POST'])
+def trial():
+    return render_template("trial.html")
+
+@app.route("/predict", methods=['POST','GET'])
+def man():
     if request.method=='POST':
-        user = request.form['nm']
-        return redirect(url_for("user", usr=user))
+        data=request.form['nm']
+        data1 = int(request.form['a'])
+        data2 = int(request.form['b'])
+        data3 = int(request.form['c'])
+        data4 = int(request.form['d'])
+        data5 = int(request.form['e'])
+        arr=np.array([[data1, data2, data3, data4, data5]])
+        pred=model.predict(arr)
+        return render_template("after.html", data=pred, name=data)
     else:
-        return render_template("login.html")
+        return render_template("trial.html")
 
 @app.route("/<usr>", methods=['POST','GET'])
 def user(usr):
